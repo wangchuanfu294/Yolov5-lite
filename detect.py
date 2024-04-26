93bcb14f3d41d3bd0cd97c242b70e1634aee99f9
wangchuanfu294 <wangchuanfu294@gmail.com>
2024-04-26 17:19:43 +0800

    Initial commit

--- detect.py
+++ detect.py
@@ -152,8 +152,8 @@ def detect(save_img=False):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default='runs/train/exp/weights/best.pt', help='model.pt path(s)')
-    parser.add_argument('--source', type=str, default="1", help='source')  # file/folder, 0 for webcam
+    parser.add_argument('--weights', nargs='+', type=str, default='weights/best.pt', help='model.pt path(s)')
+    parser.add_argument('--source', type=str, default="294.png", help='source')  # file/folder, 0 for webcam
     parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
     parser.add_argument('--conf-thres', type=float, default=0.45, help='object confidence threshold')
     parser.add_argument('--iou-thres', type=float, default=0.5, help='IOU threshold for NMS')
